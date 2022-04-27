e96218478ac133f7d7445d99b9cbe1d7605ea193
Kacper Krusinski <kacper.krusinski@microsoft.wsei.edu.pl>
2022-04-27 15:40:27 +0200

    Dodaj pliki projektów.

---
+++ lab02-kserokopiarka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab02_kserokopiarka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
